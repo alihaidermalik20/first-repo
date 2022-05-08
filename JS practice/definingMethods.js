5668fe23eb3c65085f0c33d7dbc69546d15d4f4a
alihaidermalik20 <95946965+alihaidermalik20@users.noreply.github.com>
2022-05-08 13:13:34 +0200

    try catch

--- JS practice/definingMethods.js
+++ JS practice/definingMethods.js
@@ -1,4 +1,5 @@
 // creating an object with several methods i can use
+// can use myMath.cube(2) to get 8
 
 const myMath = {
     PI: 3.14,
---
+++ JS practice/tryCatch.js
@@ -0,0 +1,11 @@
+// use try and catch to continue running the code een if an error occurs
+// it tries to print message in upper case three times but upper case can only be used with string so if you pass a number
+// it catches it and prints please pass a string instead of error and continues
+
+function yell(msg) {
+    try {
+        console.log(msg.toUpperCase().repeat(3));
+    } catch(e) {
+        console.log("Please pass a string next time");
+    }
+};
