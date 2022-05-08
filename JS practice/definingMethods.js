3c3fe9b99ec02186cf6b6a85aff009b16ae551c1
alihaidermalik20 <95946965+alihaidermalik20@users.noreply.github.com>
2022-05-08 10:16:26 +0200

    add objects with methods

---
+++ JS practice/definingMethods.js
@@ -0,0 +1,11 @@
+// creating an object with several methods i can use
+
+const myMath = {
+    PI: 3.14,
+    multiply (x,y) {
+        return x * y;
+    },
+    cube (x) {
+        return x ** 3;
+    }
+}
