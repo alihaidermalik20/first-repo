3831828cdd75166ce07fae2978e0965da18dd8a0
alihaidermalik20 <95946965+alihaidermalik20@users.noreply.github.com>
2022-05-22 00:48:50 +0200

    added regular expression to find only letters and spaces with search function

--- JS practice/regex.js
+++ JS practice/regex.js
@@ -4,9 +4,9 @@ let notMessage = "1273.";
 let empty = "";
 let space = " ";
 
-let result = message2.search(/^[A-Za-z\s]+$ && [^\s]/);
+let result = space.search(/^[A-Za-z\s]+$/);
 console.log(result);
 
 // regex is asking that from start ^ to the end $, only letters and white spaces \s occur 1 to many times + 
 // search will give the index of the first time a letter or white space is found 
-// it still allows string with only a space but 
+// it still allows string with only a space and no letters
