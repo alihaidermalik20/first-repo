5694e5399b2014dd36303f0e040ae602648e7b5f
alihaidermalik20 <95946965+alihaidermalik20@users.noreply.github.com>
2022-06-05 15:03:33 +0200

    saved

--- JS practice/nan.js
+++ JS practice/nan.js
@@ -1,5 +1,7 @@
+// NaN can't use strict equality but can be checked with isNaN function. 
+// Test 2 and 3 below will fail as even NaN === NaN evaluates to false
+
 console.assert(isNaN(undefined), 'Test 1');
-console.log(Number(undefined) === NaN, 'Test 1');
 
 console.assert(Number(null) === 0, 'Test 2');
 
