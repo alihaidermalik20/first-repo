3e0ed2d45a6d1ceda25b375dad1f7d6e1da3afcc
alihaidermalik20 <95946965+alihaidermalik20@users.noreply.github.com>
2022-07-05 17:27:00 +0200

    object values to variables

--- JS practice/destructObjects.js
+++ JS practice/destructObjects.js
@@ -9,3 +9,6 @@ const user1 = {
 // This creates 4 variables. name 'Ali' is a variable called adminName also now. 
 //died is a variable called adminDeathYear but if it's not in the object, it's N/A by default
 const {name: adminName, born: adminBirthYear, died: adminDeathYear = 'N/A', city} = user1;
+console.log(adminName); // Ali
+console.log(adminBirthYear); // 1998
+console.log(adminDeathYear); // 'N/A'
